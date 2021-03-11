e8795a40f969d0b2f76c1585e7107ab53dbfa3ac
DRAGONBALLS26 <71335022+DRAGONBALLS26@users.noreply.github.com>
2021-03-11 11:43:06 +0200

    Initial commit

---
+++ prototype/Invitatie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prototype
+{
+    class Invitatie
+    {
+        public String Catre;
+        public String Titlu;
+        public String Continut;
+        public String TrimisDe;
+        public DateTime Data;
+        public String p_Catre {
+            get { return Catre; }
+            set { Catre = value; }
+        }
+        public string p_Titlu
+        {
+            get { return Titlu; }
+            set { Titlu = value; }
+        }
+        public string p_Continut {
+            get { return Continut; }
+            set { Continut = value; }
+        }
+        public string p_TrimisDe
+        {
+            get { return TrimisDe; }
+            set { TrimisDe = value; }
+        }
+        public DateTime p_Data
+        {
+            get { return Data; }
+            set { Data = value; }
+        }
+        public Invitatie CloneMe(Invitatie obj)
+        {
+            return (Invitatie)this.MemberwiseClone();
+        }
+    }
+}
+
---
+++ prototype/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invitatie obj1 = new Invitatie();
+            obj1.p_Catre = "Dan";
+            obj1.p_Titlu = "Invitatie la ziua mea de nastere";
+            obj1.p_Continut = "Deci , baieti va invit la ziua mea de nastere!!!";
+            obj1.TrimisDe= "Dragomir";
+            obj1.p_Data = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            //Here our first object has created  
+            Invitatie[] objList = new Invitatie[6];
+            String[] nameList = { "Dan", "Cristi", "Alin", "Stefan", "Lucian", "Dummitru" };
+            int i = 0;
+            foreach (String name in nameList)
+            {
+                //objList[i] = new Invitie();  
+                objList[i] = obj1.CloneMe(obj1);
+                objList[i].p_Catre = nameList[i];
+                i++;
+            }
+            // Print all Invitation Card here  
+            foreach (Invitatie obj in objList)
+            {
+                Console.WriteLine("To :- " + obj.p_Catre);
+                Console.WriteLine("Title :- " + obj.p_Titlu);
+                Console.WriteLine("Content :- " + obj.p_Continut);
+                Console.WriteLine("Send By :- " + obj.p_TrimisDe);
+                Console.WriteLine("Date :- " + obj.Data);
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
